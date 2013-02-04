f72d865012a84c996172ace2af46f45274ce261a
root <root@gabriello.(none)>
2013-02-04 09:46:18 +0100

    Splitting for separate defined types

--- client-init/manifests/init.pp
+++ client-init/manifests/init.pp
@@ -1,4 +1,5 @@
 # Client-init, setup an installed from scratch machine with the follow:
+# * Misc (ssh, vim, git, ntp, nscd ..)
 # * Nfs
 # * Ldap (nss, idmapd)
 # * Autofs
@@ -7,27 +8,15 @@
 class client-init {
   case $operatingsystem {
     debian, ubuntu: {
-      $client_pkg = ['ldap-utils', 
-                     'ldap-auth-config', 
-                     'nfs-common',
-                     'nfs4-acl-tools'] 
-                     
-      $nss_cfg      = 'nsswitch.conf'
-      $nss_cfg_path = '/etc/nsswitch.conf'
-      
+      $misc_pkg = [ 'ssh', 'vim', 'git', 'nscd',
+                    'ntp'] 
     }
   }
 
-  package { $client_pkg :
+  package { $misc_pkg :
     ensure => installed,
   }
 
-  file { $nss_cfg :
-    ensure  => file,
-    path    => $nss_cfg_path,
-    require => Package[$client_pkg],
-    source  => "puppet:///modules/client-init/${nss_cfg}",
-  }
 } 
              
 
---
+++ client-init/manifests/nfs-krb.pp
@@ -0,0 +1,40 @@
+# Nfs configuration
+
+define client-init::nfs-krb(
+    $local_domain
+    $realm
+    ) {
+
+  include client-init
+
+  case $operatingsystem {
+    debian, ubuntu: {
+      $idmap_cfg      = 'idmapd.conf'
+      $idmap_cfg_path = '/etc/idmapd.conf'
+      $idmap_cfg_tmpl = 'client-init/idmapd.conf.deb.erb'
+  
+      $nfs_cfg        = 'nfs-common.deb'
+      $nfs_cfg_path   = '/etc/default/nfs-common'
+
+      $nfs_pkg        = ['nfs-common', 
+                        'nfs-client']
+    }
+  }
+
+  file { $idmap_cfg :
+    ensure  => file,
+    require => Package[$nfs_pkg],
+    path    => $idmap_cfg_path,
+    content => template($idmap_cfg_tmpl),
+  }
+
+  file { $nfs_cfg :
+    ensure  => file,
+    require => Package[$nfs_pkg],
+    path    => $nfs_cfg_path,
+    source  => "puppet:///modules/client-init/${nfs_cfg}",
+    
+  }
+
+}
+
